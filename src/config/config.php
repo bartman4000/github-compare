efeb5ec9400accd8f1f7a517b3efc38680e8bf7f
bartek <bartman3000@gmail.com>
2017-05-27 23:20:42 +0200

    init develop

--- src/config/config.php
+++ src/config/config.php
@@ -1,4 +1,4 @@
 <?php
 
 define("APP_HOST", "localhost:8080");
-define("GITHUB_TOKEN", "bfc1deb72f75aaf2be4b0532a8fc37213503f506");
+define("GITHUB_TOKEN", "7ffbccd1d1919e42b0a41c721e04a6aac02c8294");
